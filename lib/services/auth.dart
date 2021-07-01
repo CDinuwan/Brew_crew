9123e8b9429f0b03bf56b4dfa6f08ddb2331aa11
CDinuwan <chanukadinuwan35@gmail.com>
2021-07-01 20:51:51 +0530

    Auth setting

---
+++ lib/services/auth.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+//Sign in anon
+  Future signInAnno() async {
+    try {
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+//Sign in with email & password
+
+//Register with email & password
+
+//Sign out
+
+}
