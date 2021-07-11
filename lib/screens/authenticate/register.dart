8e07197596a18e01c0266733c4e5f64dff8366bc
CDinuwan <chanukadinuwan35@gmail.com>
2021-07-11 19:20:02 +0530

    Decoration text field

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:brew_crew/services/auth.dart';
+import 'package:brew_crew/shared/constant.dart';
 
 class Register extends StatefulWidget {
   final Function toggleView;
@@ -45,6 +46,7 @@ class __RegisterState extends State<Register> {
                   height: 20.0,
                 ),
                 TextFormField(
+                  decoration: textInputDecoration.copyWith(hintText: 'Email'),
                   validator: (val) => val.isEmpty ? 'Enter an email' : null,
                   onChanged: (val) {
                     setState(() => email = val);
@@ -52,6 +54,8 @@ class __RegisterState extends State<Register> {
                 ),
                 SizedBox(height: 20.0),
                 TextFormField(
+                  decoration:
+                      textInputDecoration.copyWith(hintText: 'Password'),
                   obscureText: true,
                   validator: (val) =>
                       val.length < 6 ? 'Enter a password 6+ chars long!' : null,
